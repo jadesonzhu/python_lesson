8efb820c6450f601db1c23a70028bb3cdbc580a7
zhuyubao <jadesonzhu@163.com>
2026-01-03 15:27:56 +0800

    main function

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+print(f"numpy version is {np.__version__}")
+print(f"pandas version is {pd.__version__}")
+
+if __name__ == "__main__":
+    print("This is the main module.")
